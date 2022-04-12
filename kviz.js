b14b3fcd51dbb57020d9a96a089817ea4fc2aa76
Kateřina Táborská <ktaborska92@gmail.com>
2022-04-12 22:20:36 +0200

    adding onclick event

--- kviz.js
+++ kviz.js
@@ -46,7 +46,12 @@ function zobrazOtazku(indexOtazky) {
 
 // Funkce se postará o obsluhu kliknutí na odpověď
 // Musíme ji navázat na kokrétní odpovědi každé otázky (to uděláme v rámci funkce zobrazOtazku())
-function klikNaOdpoved() {}
+function klikNaOdpoved() {
+    indexOtazky++;
+    zobrazOtazku(indexOtazky);
+}
+
+
 
 // Když už mám odpovězeno na vše (řídí se velikosí objektu otazky na řádku 3), tak mohu zobrazi výsledky
 // Vypočítám skóre a nageneruje nové elementy do HTML
