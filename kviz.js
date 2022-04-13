e3812a1fbf1b13c1d2f8ec8beae5df9581e93d61
Kateřina Táborská <ktaborska92@gmail.com>
2022-04-13 15:04:04 +0200

    adding function zobrazVyhodnoceni

--- kviz.js
+++ kviz.js
@@ -23,14 +23,18 @@ const otazkyKviz = [
         }
 ];
 
-// Dále budeš potřebovat další proměnné - jaké?
 let indexOtazky = 0;
 let zaznamyOdpovedi = [];
 
-// Tato funkce se postará o vygenerování otázky
-// Zavoláme ji jednou na začátku a poté vždy po odpovězení
-zobrazOtazku(indexOtazky)
-function zobrazOtazku(indexOtazky) {
+zobrazKviz(indexOtazky);
+
+function zobrazKviz(indexOtazky) {
+
+    if(indexOtazky >= 3){
+        zobrazVyhodnoceni();
+        return false;
+    }
+    
     document.querySelector('#poradi').textContent = otazkyKviz[indexOtazky].poradi;
 
     document.querySelector('#obrazek').src = otazkyKviz[indexOtazky].obrazek;
@@ -45,20 +49,16 @@ function zobrazOtazku(indexOtazky) {
     }  
 }
 
-// Funkce se postará o obsluhu kliknutí na odpověď
-// Musíme ji navázat na kokrétní odpovědi každé otázky (to uděláme v rámci funkce zobrazOtazku())
 function klikNaOdpoved(indexOdpovedi) {
     indexOtazky++;
-    zobrazOtazku(indexOtazky);
+    zobrazKviz(indexOtazky);
 
     zaznamyOdpovedi.push(indexOdpovedi);
     console.log(indexOdpovedi)
     
 }
 
-let odpoved = 
-
-// Když už mám odpovězeno na vše (řídí se velikosí objektu otazky na řádku 3), tak mohu zobrazi výsledky
-// Vypočítám skóre a nageneruje nové elementy do HTML
-// Touto funkcí končí můj program (budu se rozhodovat, zda ji zavolat v rámci klikNaOdpoved())
-function zobrazVyhodnoceni() {}
+function zobrazVyhodnoceni (){
+    document.querySelector('.kviz').style.display = 'none';
+    document.querySelector('.vyhodnoceni').style.display = 'block';
+}
