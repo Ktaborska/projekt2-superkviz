b1bec540363c75ebe19682501ca69b2de9d63e86
Kateřina Táborská <ktaborska92@gmail.com>
2022-04-13 14:35:31 +0200

    saving answers

--- kviz.js
+++ kviz.js
@@ -25,6 +25,7 @@ const otazkyKviz = [
 
 // Dále budeš potřebovat další proměnné - jaké?
 let indexOtazky = 0;
+let zaznamyOdpovedi = [];
 
 // Tato funkce se postará o vygenerování otázky
 // Zavoláme ji jednou na začátku a poté vždy po odpovězení
@@ -46,12 +47,16 @@ function zobrazOtazku(indexOtazky) {
 
 // Funkce se postará o obsluhu kliknutí na odpověď
 // Musíme ji navázat na kokrétní odpovědi každé otázky (to uděláme v rámci funkce zobrazOtazku())
-function klikNaOdpoved() {
+function klikNaOdpoved(indexOdpovedi) {
     indexOtazky++;
     zobrazOtazku(indexOtazky);
+
+    zaznamyOdpovedi.push(indexOdpovedi);
+    console.log(indexOdpovedi)
+    
 }
 
-
+let odpoved = 
 
 // Když už mám odpovězeno na vše (řídí se velikosí objektu otazky na řádku 3), tak mohu zobrazi výsledky
 // Vypočítám skóre a nageneruje nové elementy do HTML
